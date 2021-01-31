668e7cffb4c4c37c36b54ef46cbfced65a2dd330
a.sploshnov <ksander@yandex.ru>
2021-01-31 16:34:55 +0300

    add files  less_1_SPL_01.js /  less_1_SPL_02.js

---
+++ less_1_SPL_01.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var Tc = prompt("Введите температуру по Цельсию", "");
+var Tf = (9 / 5) * Tc + 32;
+
+alert(Tf);
---
+++ less_1_SPL_02.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var admin;
+var name = "Василий";
+
+admin = name;
+
+console.log(admin);
