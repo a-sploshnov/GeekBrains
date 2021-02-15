6cfa1969f164e2666d9a685cf27093abddd0b171
a.sploshnov <ksander@yandex.ru>
2021-02-15 20:52:32 +0300

    lesson_3 final

---
+++ lesson3/less_3_1.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// С помощью цикла while вывести все простые числа в промежутке от 0 до 100
+
+let isPrime = function (num) {
+    let i = 2;
+
+    while (i < num) {
+        if (num % i === 0) {
+            return false;
+        }
+        i++;
+    }
+    return num > 1;
+}
+
+let primesList = function (int) {
+    let arrPrimes = [];
+    let z = 2;
+
+    while (z <= int) {
+        if (isPrime(z)) {
+            arrPrimes.push(z);
+        }
+        z++;
+    }
+
+    return arrPrimes;
+}
+
+primesList(100);
---
+++ lesson3/less_3_2.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//  Товары в корзине хранятся в массиве. Задачи:
+// a) Организовать такой массив для хранения товаров в корзине.
+
+let arr = [1800, 2300, 5200, 738, 999, 2999, 850, 7500];
+let sumArr = 0;
+
+// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+function countBasketPrice(array) {
+    for (let i = 0; i < array.length; i++) {
+        sumArr += array[i];
+    }
+    return sumArr;
+
+}
+
+console.log(countBasketPrice(arr));
+
+
